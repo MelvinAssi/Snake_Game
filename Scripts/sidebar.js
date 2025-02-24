63f643758b4090f1df621acb995d7b0cb6622911
unknown <melvinassi.pro@gmail.com>
2025-02-24 18:34:08 +0100

    add html and sidebar css/js

---
+++ Scripts/sidebar.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById("toggle-btn")
+    const sidebar=document.getElementById("sidebar")
+    const sidebarState = localStorage.getItem('sidebarState');
+
+    if (sidebarState === 'closed' && !(sidebar.classList.contains('close'))) {
+        sidebar.classList.add('close');
+    }
+    setTimeout(() => {
+        sidebar.style.transition = 'width 300ms ease-in-out, padding 300ms ease-in-out'; // Active la transition après le délai
+    }, 10);
+
+    toggleButton.addEventListener('click', () => {
+        
+        sidebar.classList.toggle('close');
+        toggleButton.classList.toggle("rotate")
+
+        if (sidebar.classList.contains('close')) {
+            localStorage.setItem('sidebarState', 'closed');
+        } else {
+            localStorage.setItem('sidebarState', 'open');
+        }
+    });
+});
